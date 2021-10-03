8838b09315eaf9696e574232886e005b76be6fb0
Jalal Sordo <jalal_sordo@hotmail.com>
2021-10-03 03:27:37 +0100

    text box validation , decimals only

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
 import "./App.css";
 import React from "react";
 
+const rx_live = /^[+-]?\d*(?:[.,]\d*)?$/;
+
 class App extends React.Component {
 
   constructor(props) {
     super(props)
     this.state = {
-      manager: "test-Manager",
+      manager: "0x000ebc",
       participants: [],
       prize: 0,
       amount: 0,
-      winner: "test-winner",
+      winner: "0x000ebc",
     }
     this.handleChange = this.handleChange.bind(this)
     this.enterLottery = this.enterLottery.bind(this)
     this.pickWinner = this.pickWinner.bind(this)
   }
 
-  handleChange(event) {
-    console.log(event.target.name, event.target.value)
-    this.setState({
-      [event.target.name]: event.target.value
-    })
+  handleChange(evt) {
+    if (rx_live.test(evt.target.value))
+      this.setState({ amount: evt.target.value });
   }
 
   enterLottery() {
@@ -41,7 +41,10 @@ class App extends React.Component {
         <hr></hr>
         <br></br>
         <h4><p>This contract is managed by <span className="important">{this.state.manager}</span></p></h4>
-        <h4><p>There are currently <span className="important">{this.state.participants.length}</span> people entered, competing to win <span className="important">{this.state.prize} ether</span></p></h4>
+        <h4><p>There are currently <span className="important">
+          {this.state.participants.length}
+        </span> people entered, competing to win <span className="important">
+            {this.state.prize} ether</span></p></h4>
         <br></br>
         <h2>Want to try your luck?</h2>
         <div className="row g-3 align-items-center">
@@ -52,6 +55,8 @@ class App extends React.Component {
             <input type="text" id="amount" name="amount"
               value={this.state.amount}
               onChange={this.handleChange}
+              pattern="[+-]?\d+(?:[.,]\d+)?"
+              placeholder="Enter amount"
               className="form-control" aria-describedby="amountHelpInline" />
           </div>
           <div className="col-auto">
@@ -62,14 +67,16 @@ class App extends React.Component {
 
         </div>
         <div className="align-items-center">
-          <input type="button" value="Enter" className="btn btn-primary btn-lg" onClick={this.enterLottery} />
+          <input type="button" value="Enter" className="btn btn-primary btn-lg"
+            onClick={this.enterLottery} />
         </div>
         <hr></hr>
         <br></br>
         <h2>Time to pick a winner</h2>
         <br></br>
         <div className="align-items-center">
-          <input type="button" value="Pick a Winner" className="btn btn-success btn-lg" onClick={this.pickWinner} />
+          <input type="button" value="Pick a Winner" className="btn btn-success btn-lg"
+            onClick={this.pickWinner} />
         </div>
         <br></br>
         <h2>{this.state.winner} has won!</h2>
